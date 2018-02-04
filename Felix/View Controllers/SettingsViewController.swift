e0cd24a59b3512c62e4262089817b9a1e13d6b6f
insomnolent <jbcccsun@gmail.com>
2018-02-04 06:06:02 -0800

    changed name

--- Felix/View Controllers/SettingsViewController.swift
+++ Felix/View Controllers/SettingsViewController.swift
@@ -43,7 +43,7 @@ class SettingsViewController: UIViewController {
         let aboutTextView = UITextView(frame: CGRect(x: 20, y: yOrigin, width: view.frame.width - 40, height: aboutHeight))
         
         aboutTextView.font = UIFont(name: "Poppins-Regular", size: 14)
-        aboutTextView.text = "Val (short for Valerian) utilizes cognitive behavioural therapy (CBT) to help users as much as possible! CBT is a mental exercise proven to be one of the best methods to improve daily mood and overall health. The key steps in CBT is to: identify and get to know automatic negative thoughts that are spontaneously triggered, ask whether there is concrete evidence or good reason to feel the way you do, challenge your initial thoughts by doubting your instincts, and find an alternative to view the thought or situation."
+        aboutTextView.text = "Val utilizes cognitive behavioural therapy (CBT) to help users as much as possible! CBT is a mental exercise proven to be one of the best methods to improve daily mood and overall health. The key steps in CBT is to: identify and get to know automatic negative thoughts that are spontaneously triggered, ask whether there is concrete evidence or good reason to feel the way you do, challenge your initial thoughts by doubting your instincts, and find an alternative to view the thought or situation."
         
         regularView.addSubview(aboutTextView)
         yOrigin += aboutHeight
@@ -64,7 +64,7 @@ class SettingsViewController: UIViewController {
         let helpTextView = UITextView(frame: CGRect(x: 20, y: yOrigin, width: view.frame.width - 40, height: helpHeight))
         
         helpTextView.font = UIFont(name: "Poppins-Regular", size: 14)
-        helpTextView.text = "Val is not a replacement for a real therapist or psychologist. If you ever need to talk to someone for anonymous emotional support, please call (212) 673-3000. Free, immediately accessible, confidential, 24/7."
+        helpTextView.text = "Valerian is not a replacement for a real therapist or psychologist. If you ever need to talk to someone for anonymous emotional support, please call (212) 673-3000. Free, immediately accessible, confidential, 24/7."
         
         regularView.addSubview(helpTextView)
     }
